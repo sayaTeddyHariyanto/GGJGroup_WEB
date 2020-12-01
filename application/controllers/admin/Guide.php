bf00459a705f00922305f02d174668d98211c2da
Muhammad Wildan Aulia Kahfi <55533786+WildanE41181398@users.noreply.github.com>
2020-12-01 19:34:11 +0700

    Dashboard admin

--- application/controllers/admin/Guide.php
+++ application/controllers/admin/Guide.php
@@ -2,12 +2,29 @@
 
 class Guide extends CI_Controller 
 {
+
+    function __construct()
+  {
+    parent::__construct();
+    // if ($this->session->userdata('nama') == '') {
+    //   redirect('admin/login/');
+    // }
+    $this->load->model('M_dashboard');
+  }
+  
     function index()
     {
+        $data['anggota'] = $this->M_dashboard->getAll();
+        //$data['history'] = $this->M_dashboard->getHistory();
+        $data['masukan']= $this->M_dashboard->getBulananPembayaran();
+        $data['distribusi']= $this->M_dashboard->getBulananPendistribusian();
+        $data['saldo']= $this->M_dashboard->getSisaSaldo();
+        //$data['favorit']= $this->M_dashboard->getFavorit()->result();
+        //$data['grafik']= $this->M_dashboard->getMingguan()->result();
         $this->load->view('templates/admin_header');
         $this->load->view('templates/admin_sidebar');
-		$this->load->view('templates/admin_navbar');
-		$this->load->view('admin/guide');
+		    $this->load->view('templates/admin_navbar');
+		    $this->load->view('admin/guide', $data);
         $this->load->view('templates/admin_footer_js');
         $this->load->view('templates/admin_custom_js');
         $this->load->view('templates/admin_footer');
