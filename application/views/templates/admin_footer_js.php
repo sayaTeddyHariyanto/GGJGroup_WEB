33f0ea6e63b7ef593594f870fe096b4f33033887
NUR-HADI-E41181216 <aryajavadd09@gmail.com>
2020-12-21 21:43:48 +0700

    fitur upload file
    fitur upload file

--- application/views/templates/admin_footer_js.php
+++ application/views/templates/admin_footer_js.php
@@ -51,10 +51,6 @@
     <!-- Page level plugins -->
     <script src="<?= base_url() ?>assets/admin/vendor/chart.js/Chart.min.js"></script>
 
-    <!-- Page level custom scripts -->
-    <script src="<?= base_url() ?>assets/admin/js/demo/chart-area-demo.js"></script>
-    <script src="<?= base_url() ?>assets/admin/js/demo/chart-pie-demo.js"></script>
-
     <!-- Page level plugins -->
     <script src="<?= base_url() ?>assets/admin/vendor/datatables/jquery.dataTables.min.js"></script>
     <script src="<?= base_url() ?>assets/admin/vendor/datatables/dataTables.bootstrap4.min.js"></script>
