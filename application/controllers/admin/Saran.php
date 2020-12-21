5893e4ad9c2f92d12c4fdb9da127fd5eba5f97d7
HafizAldion <55680951+HafizAldion@users.noreply.github.com>
2020-12-22 01:26:40 +0700

    Controller Saran

---
+++ application/controllers/admin/Saran.php
@@ -0,0 +1,70 @@
+<?php
+
+// Class Zakat
+class Saran extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_crud');
+        // $this->load->library('Primslib');
+
+        if ($this->session->userdata('status') == '') {
+            redirect('admin/login');
+        }
+    }
+
+    function index()
+    {
+        $data['saran'] = $this->m_crud->getAll('tb_saran')->result();
+        $this->load->view('templates/admin_header');
+        $this->load->view('templates/admin_sidebar');
+        $this->load->view('templates/admin_navbar');
+        $this->load->view('admin/saran', $data);
+        $this->load->view('templates/admin_footer_js');
+        $this->load->view('templates/admin_custom_js');
+        $this->load->view('templates/admin_footer');
+    }
+
+    function detail($id)
+    {
+        $data['saran'] = $this->m_crud->edit(['id_saran' => $id], 'tb_saran')->result();
+        $this->load->view('templates/admin_header');
+        $this->load->view('templates/admin_sidebar');
+        $this->load->view('templates/admin_navbar');
+        $this->load->view('admin/saran_detail', $data);
+        $this->load->view('templates/admin_footer_js');
+        $this->load->view('templates/admin_custom_js');
+        $this->load->view('templates/admin_footer');
+    }
+
+    function hapus($id)
+    {
+        $where = array(
+            'id_saran' => $id
+        );
+
+        $this->m_crud->delete($where, 'tb_saran');
+        if ($this->db->affected_rows() == true) {
+            $this->session->set_flashdata('pesan', '
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>Selamat!</strong> Anda berhasil menghapus data.
+                <button type="button" class="close py-auto" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            ');
+            redirect("admin/saran");
+        } else {
+            $this->session->set_flashdata('pesan', '
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Maaf!</strong> Anda gagal menghapus data.
+                <button type="button" class="close py-auto" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            ');
+            redirect("admin/saran");
+        }
+    }
+}
